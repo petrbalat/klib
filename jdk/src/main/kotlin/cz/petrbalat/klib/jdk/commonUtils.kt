0e5731d585be5bfc4b9e8d58f7f2ac7d5f6c2c7d
Petr Balat <petr.balat@gmail.com>
2019-12-18 19:14:46 +0100

    0.3.2

---
+++ jdk/src/main/kotlin/cz/petrbalat/klib/jdk/commonUtils.kt
@@ -0,0 +1,25 @@
+package cz.petrbalat.klib.jdk
+
+import java.util.*
+
+/**
+ * Created by Petr Balat
+ */
+fun Boolean.toBooleanInt(): Int = if(this) 1 else 0
+
+fun Throwable.causeSequence(): Sequence<Throwable> = generateSequence(this, {
+    if (it.cause == it) {
+        return@generateSequence null
+    }
+    it.cause
+})
+
+inline fun <T> tryOn(action: () -> T?): T? = try {
+    action()
+} catch (ex: Throwable) {
+    null
+}
+
+infix fun Double.relativelyEqual(other:Double):Boolean = Math.abs(this - other) <= 1E-8
+
+fun <T> Optional<T>.orNull(): T? = orElse(null)
