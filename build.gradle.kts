5c0577b741aaf1d2c64a0a1494fd44b7db152faf
Petr Balat <petr.balat@gmail.com>
2020-01-17 08:57:00 +0100

    spring boot 2.2.3.RELEASE

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 }
 
 plugins {
-    id("org.springframework.boot") version "2.2.1.RELEASE" apply false
+    id("org.springframework.boot") version "2.2.3.RELEASE" apply false
     id("io.spring.dependency-management") version "1.0.8.RELEASE" apply false
     kotlin("jvm") version "1.3.61" apply false
     kotlin("plugin.spring") version "1.3.61" apply false
