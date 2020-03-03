e0ac3a4013b990e9c346ca10429c89273982c56d
Petr Balat <petr.balat@gmail.com>
2020-03-03 21:38:02 +0100

    kotlin 1.3.70
    - https://blog.jetbrains.com/kotlin/2020/03/kotlin-1-3-70-released/

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 plugins {
     id("org.springframework.boot") version "2.2.5.RELEASE" apply false
     id("io.spring.dependency-management") version "1.0.8.RELEASE" apply false
-    kotlin("jvm") version "1.3.61" apply false
+    kotlin("jvm") version "1.3.70" apply false
     kotlin("plugin.spring") version "1.3.61" apply false
 }
 
