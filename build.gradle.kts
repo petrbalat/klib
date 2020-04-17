d47349e3b213fd1cc132477b33ed155243a6adfb
Petr Balat <petr.balat@gmail.com>
2020-04-17 16:46:31 +0200

    kotlin 1.3.72

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ repositories {
 plugins {
     id("org.springframework.boot") version "2.2.6.RELEASE" apply false
     id("io.spring.dependency-management") version "1.0.8.RELEASE" apply false
-    kotlin("jvm") version "1.3.71" apply false
-    kotlin("plugin.spring") version "1.3.71" apply false
+    kotlin("jvm") version "1.3.72" apply false
+    kotlin("plugin.spring") version "1.3.72" apply false
 }
 
 allprojects {
