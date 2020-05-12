4b5c0091ef4555b481eaab09fb5fb20cfe7aad13
Petr Balat <balat@oksystem.cz>
2020-05-12 08:12:46 +0200

    gradle 6.4

--- jdk/build.gradle.kts
+++ jdk/build.gradle.kts
@@ -1,27 +1,18 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-
 plugins {
-    id("org.springframework.boot")
+//    id("org.springframework.boot") // kvůli
     kotlin("jvm")
-}
-
-tasks.getByName<Jar>("jar") {
-    enabled = true
-}
-tasks.getByName<BootJar>("bootJar") {
-    enabled = false
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 //    implementation(kotlin("reflect"))
 
-    compileOnly("com.fasterxml.jackson.core:jackson-databind")
-    compileOnly("org.slf4j:slf4j-api")
+    compileOnly("com.fasterxml.jackson.core:jackson-databind:2.11.0")
+    compileOnly("org.slf4j:slf4j-api:1.7.30")
 
-    testImplementation("com.fasterxml.jackson.core:jackson-databind")
-    testImplementation("org.slf4j:slf4j-api")
-    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    testImplementation("org.slf4j:slf4j-api:1.7.30")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.11.0")
+    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
     testImplementation(kotlin("test-junit5"))
 }
