9954265f7ff5f3d4bf0246d0194296e7cc251f29
Petr Balat <petr.balat@gmail.com>
2020-06-12 08:57:39 +0200

    spring boot 2.3.1
    - https://github.com/spring-projects/spring-boot/releases/tag/v2.3.1.RELEASE

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 plugins {
-    id("org.springframework.boot") version "2.3.0.RELEASE" apply false
+    id("org.springframework.boot") version "2.3.1.RELEASE" apply false
     id("io.spring.dependency-management") version "1.0.9.RELEASE" apply false
     kotlin("jvm") version "1.3.72" apply false
     kotlin("plugin.spring") version "1.3.72" apply false
