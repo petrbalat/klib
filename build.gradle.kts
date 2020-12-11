821609f1090bc4f06862a3b289ae2a0c46439923
Petr Balat <petr.balat@gmail.com>
2020-12-11 11:52:53 +0100

    spring boot 2.4.1
    - https://spring.io/blog/2020/12/11/spring-boot-2-4-1-available-now
    - https://github.com/spring-projects/spring-boot/releases/tag/v2.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,16 +16,16 @@ repositories {
 }
 
 plugins {
-    id("org.springframework.boot") version "2.4.0" apply false
+    id("org.springframework.boot") version "2.4.1" apply false
     id("io.spring.dependency-management") version "1.0.10.RELEASE" apply false
     id("com.jfrog.bintray") version "1.8.5" apply true
-    kotlin("jvm") version "1.4.20" apply false
-    kotlin("plugin.spring") version "1.4.20" apply false
+    kotlin("jvm") version "1.4.21" apply false
+    kotlin("plugin.spring") version "1.4.21" apply false
 }
 
 allprojects {
     group = "com.github.petrbalat.klib"
-    version = "0.10.4"
+    version = "0.10.5"
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = "1.8"
