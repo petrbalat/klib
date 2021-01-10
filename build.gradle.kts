04b4ef85c63691ae3e5aa46a24682fd823e3e2a6
Petr Balat <petr.balat@gmail.com>
2021-01-10 14:01:31 +0100

    0.10.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ plugins {
 
 allprojects {
     group = "com.github.petrbalat.klib"
-    version = "0.10.5"
+    version = "0.10.6"
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = "1.8"
