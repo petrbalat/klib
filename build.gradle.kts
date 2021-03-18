f987f83bc2dbc98bf3f3c167a6c0f567ff2f75bd
Petr Balat <petr.balat@gmail.com>
2021-03-18 12:17:34 +0100

    spring boot 2.4.4
    - https://github.com/spring-projects/spring-boot/releases/tag/v2.4.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,16 +16,16 @@ repositories {
 }
 
 plugins {
-    id("org.springframework.boot") version "2.4.3" apply false
+    id("org.springframework.boot") version "2.4.4" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
     id("com.jfrog.bintray") version "1.8.5" apply true
-    kotlin("jvm") version "1.4.30" apply false
-    kotlin("plugin.spring") version "1.4.30" apply false
+    kotlin("jvm") version "1.4.31" apply false
+    kotlin("plugin.spring") version "1.4.31" apply false
 }
 
 allprojects {
     group = "com.github.petrbalat.klib"
-    version = "0.13.0"
+    version = "0.2.0"
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = "1.8"
