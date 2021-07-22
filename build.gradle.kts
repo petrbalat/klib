171270caf312bd8e2b32ec9dd63fb4ac8d1f407e
Petr Balat <petr.balat@gmail.com>
2021-07-22 18:15:16 +0200

    0.5.5 - boot 2.5.2
    - https://github.com/spring-projects/spring-boot/releases/tag/v2.5.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 }
 
 plugins {
-    id("org.springframework.boot") version "2.5.2" apply false
+    id("org.springframework.boot") version "2.5.3" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
     kotlin("jvm") version "1.5.21" apply false
     kotlin("plugin.spring") version "1.5.21" apply false
@@ -24,7 +24,7 @@ plugins {
 
 allprojects {
     group = "io.github.petrbalat"
-    version = "0.5.4"
+    version = "0.5.5"
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = "1.8"
