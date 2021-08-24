36dcc45cc19d1a9f5f92139811b93ecdc642d962
Petr Balat <balat@oksystem.cz>
2021-08-24 16:33:31 +0200

    0.5.9 - kotlin 1.5.30

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,13 @@ repositories {
 plugins {
     id("org.springframework.boot") version "2.5.4" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
-    kotlin("jvm") version "1.5.21" apply false
-    kotlin("plugin.spring") version "1.5.21" apply false
+    kotlin("jvm") version "1.5.30" apply false
+    kotlin("plugin.spring") version "1.5.30" apply false
 }
 
 allprojects {
     group = "io.github.petrbalat"
-    version = "0.5.8"
+    version = "0.5.9"
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = "1.8"
