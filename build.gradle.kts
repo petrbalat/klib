d3794543f3d41e730ace8a8fc95632b31da7058b
Petr Balat <balat@oksystem.cz>
2021-12-14 10:21:46 +0100

    0.13.0 - kotlin 1.6.10
    - https://github.com/JetBrains/kotlin/releases/tag/v1.6.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,13 +16,13 @@ repositories {
 plugins {
     id("org.springframework.boot") version "2.6.1" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
-    kotlin("jvm") version "1.6.0" apply false
-    kotlin("plugin.spring") version "1.6.0" apply false
+    kotlin("jvm") version "1.6.10" apply false
+    kotlin("plugin.spring") version "1.6.10" apply false
 }
 
 allprojects {
     group = "io.github.petrbalat"
-    version = "0.12.2"
+    version = "0.13.0"
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = "1.8"
