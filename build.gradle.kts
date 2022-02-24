af74b4e8497722f0e7d6a16155221f91b7eead1b
Petr Balat <petr.balat@gmail.com>
2022-02-24 18:12:54 +0100

    0.16.0 - spring boot 2.6.4
    - https://spring.io/blog/2022/02/24/spring-boot-2-6-4-available-now

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 plugins {
-    id("org.springframework.boot") version "2.6.3" apply false
+    id("org.springframework.boot") version "2.6.4" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
     kotlin("jvm") version "1.6.10" apply false
     kotlin("plugin.spring") version "1.6.10" apply false
@@ -20,7 +20,7 @@ plugins {
 
 allprojects {
     group = "io.github.petrbalat"
-    version = "0.15.5"
+    version = "0.16.0"
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = "1.8"
