31393f9ce6a562015fe07a21ed32a1a0595f452e
Petr Balat <petr.balat@gmail.com>
2022-03-31 18:19:22 +0200

    graphql

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,6 @@ pluginManagement {
 }
 
 rootProject.name = "klib"
-include("spring-boot-jwt", "spring-boot-web", "spring-boot-webflux", "spring-boot-mail", "spring-boot-nats", "spring-boot-image", "geom", "retrofit2", "jdk", "pdf", "qr", "ftp")
+include("spring-boot-jwt", "spring-boot-web", "spring-boot-webflux", "spring-boot-mail",
+    "spring-boot-nats", "spring-boot-image", "spring-boot-image",  "spring-boot-graphql", "geom", "retrofit2", "jdk", "pdf", "qr", "ftp")
 
