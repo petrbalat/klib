57224bfd0b540a683a595dec3ee14c6b26cf8abc
Petr Balat <petr.balat@gmail.com>
2022-07-08 13:29:18 +0200

    0.52.0 - kotlin 1.7.10
    - https://github.com/JetBrains/kotlin/releases/tag/v1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,13 +14,13 @@ repositories {
 plugins {
     id("org.springframework.boot") version "2.7.0" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
-    kotlin("jvm") version "1.7.0" apply false
-    kotlin("plugin.spring") version "1.7.0" apply false
+    kotlin("jvm") version "1.7.10" apply false
+    kotlin("plugin.spring") version "1.7.10" apply false
 }
 
 allprojects {
     group = "io.github.petrbalat"
-    version = "0.51.4"
+    version = "0.52.0"
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = "1.8"
