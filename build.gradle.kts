e96e1466e6be44d67e452784a578bd34a44883cb
Petr Balat <petr.balat@gmail.com>
2022-11-14 17:48:50 +0100

    0.70.0 - kotlin 1.7.21 a boot 2.7.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,15 +12,15 @@ repositories {
 }
 
 plugins {
-    id("org.springframework.boot") version "2.7.4" apply false
-    id("io.spring.dependency-management") version "1.0.14.RELEASE" apply false
-    kotlin("jvm") version "1.7.20" apply false
-    kotlin("plugin.spring") version "1.7.20" apply false
+    id("org.springframework.boot") version "2.7.5" apply false
+    id("io.spring.dependency-management") version "1.0.15.RELEASE" apply false
+    kotlin("jvm") version "1.7.21" apply false
+    kotlin("plugin.spring") version "1.7.21" apply false
 }
 
 allprojects {
     group = "io.github.petrbalat"
-    version = "0.65.0"
+    version = "0.70.0"
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = "1.8"
