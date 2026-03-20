68d672816300f4e95c775e9cf0581d25e6956d7e
Petr Balat <petr.balat@gmail.com>
2026-03-20 19:22:04 +0100

    fix JwtMvcSecurityConfiguration.kt

--- spring-boot-jwt/src/main/kotlin/cz/petrbalat/klib/spring/jwt/mvc/JWTAuthenticationFilter.kt
+++ spring-boot-jwt/src/main/kotlin/cz/petrbalat/klib/spring/jwt/mvc/JWTAuthenticationFilter.kt
@@ -3,12 +3,14 @@ package cz.petrbalat.klib.spring.jwt.mvc
 /**
  * @see https://auth0.com/blog/implementing-jwt-authentication-on-spring-boot/
  */
-import com.fasterxml.jackson.databind.ObjectMapper
 import cz.petrbalat.klib.jdk.datetime.now
 import cz.petrbalat.klib.jdk.datetime.toDate
 import cz.petrbalat.klib.spring.jwt.HEADER_STRING
 import cz.petrbalat.klib.spring.jwt.TOKEN_PREFIX
 import io.jsonwebtoken.Jwts
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
@@ -16,11 +18,9 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import toKey
+import tools.jackson.databind.json.JsonMapper
 import java.time.Duration
 import java.util.*
-import jakarta.servlet.FilterChain
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
 
 /**
  * Přihlášení
@@ -36,7 +36,7 @@ import jakarta.servlet.http.HttpServletResponse
  * }
  */
 open class JWTAuthenticationFilter(private @Value("\${jwt.token.secret}") val secret: String,
-                                   private val mapper: ObjectMapper,
+                                   private val mapper: JsonMapper,
                                    private val managerBulder: AuthenticationManagerBuilder,
                                    val expirationDateTime: Duration,
                                    private val userPrepare: PrepareUserToJson<UserDetails>?) : UsernamePasswordAuthenticationFilter() {
--- spring-boot-jwt/src/main/kotlin/cz/petrbalat/klib/spring/jwt/mvc/JacksonSerializerDeserializer.kt
+++ spring-boot-jwt/src/main/kotlin/cz/petrbalat/klib/spring/jwt/mvc/JacksonSerializerDeserializer.kt
@@ -1,16 +1,16 @@
 package cz.petrbalat.klib.spring.jwt.mvc
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import io.jsonwebtoken.io.Deserializer
 import io.jsonwebtoken.io.Serializer
+import tools.jackson.databind.json.JsonMapper
 
-class JacksonSerializer(private val mapper: ObjectMapper) : Serializer<MutableMap<String, *>?> {
+class JacksonSerializer(private val mapper: JsonMapper) : Serializer<MutableMap<String, *>?> {
     override fun serialize(map: MutableMap<String, *>?): ByteArray {
         return mapper.writeValueAsBytes(map)
     }
 }
 
-class JacksonDeserializer(private val mapper: ObjectMapper) : Deserializer<MutableMap<String, *>?> {
+class JacksonDeserializer(private val mapper: JsonMapper) : Deserializer<MutableMap<String, *>?> {
 
     override fun deserialize(bytes: ByteArray): MutableMap<String, *>? {
         val clazz: Class<Any> = MutableMap::class.java as Class<Any>
